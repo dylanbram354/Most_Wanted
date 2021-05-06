78e31e234174bec85d0f492bc9628da2b8ba33e6
dylanbram354 <dylanbram354@gmail.com>
2021-05-06 14:28:36 -0500

    Created function to append table with new array

--- app.js
+++ app.js
@@ -54,3 +54,51 @@ function makeTable(array){ //function that takes in array of objects
 makeTable(people);
 
 //later on if we change the "people" array, we can use the same "makeTable" function to generate a new table with the new array
+
+function addToTable(array){
+    let table = document.getElementById("table");
+    
+    for (let i=0; i<array.length; i++){
+        let newRow = document.createElement("tr");
+        table.appendChild(newRow);
+        let object = array[i];
+
+        for (let e in object){
+            let newCell = document.createElement("td");
+            newRow.appendChild(newCell)
+
+            newCell.innerHTML = object[e];
+        }
+    }
+}
+
+// let newPeople=[
+//     {
+// 		"id": 272822514,
+// 		"firstName": "Billy",
+// 		"lastName": "Bob",
+// 		"gender": "male",
+// 		"dob": "1/18/1949",
+// 		"height": 71,
+// 		"weight": 175,
+// 		"eyeColor": "brown",
+// 		"occupation": "programmer",
+// 		"parents": [],
+// 		"currentSpouse": 401222887
+// 	},
+// 	{
+// 		"id": 401222887,
+// 		"firstName": "Uma",
+// 		"lastName": "Bob",
+// 		"gender": "female",
+// 		"dob": "4/1/1947",
+// 		"height": 65,
+// 		"weight": 162,
+// 		"eyeColor": "brown",
+// 		"occupation": "assistant",
+// 		"parents": [],
+// 		"currentSpouse": 272822514
+// 	}
+// ]
+
+// addToTable(newPeople); //success!
