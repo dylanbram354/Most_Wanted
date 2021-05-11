bffa8d6ba8ee60d4c785b738f30f156c965c6932
lyolayale <lyolayale19@gmail.com>
2021-05-11 09:35:27 -0500

    testing commit

--- app.js
+++ app.js
@@ -16,103 +16,102 @@ function searchByLastName() {
 	}
 }
 
-function searchByMultiple(){
-    let idInput = parseInt(document.forms['all-criteria']['id'].value);
-    let firstNameInput = document.forms['all-criteria']['fname'].value;
-    let lastNameInput = document.forms['all-criteria']['lname'].value;
-    let genderInput = document.forms['all-criteria']['gender'].value;
-    let dobInput = document.forms['all-criteria']['dob'].value;
-    let heightInput = parseInt(document.forms['all-criteria']['height'].value);
-    let weightInput = parseInt(document.forms['all-criteria']['weight'].value);
-    let eyeColorInput = document.forms['all-criteria']['eyeColor'].value;
-    let occupationInput = document.forms['all-criteria']['occupation'].value;
-    // let parentsInput = document.forms['all-criteria']['parents'].value;
-    //     if (parentsInput !== ""){
-    //         parentsInput = parentsInput.split(',');
-    //         let parentsInputNumArray = [];
-    //         for (let i=0; i<parentsInput.length; i++){
-    //             parentsInputNumArray.push(parseInt(parentsInput[i]))
-    //         }
-    //         parentsInput = parentsInputNumArray;
-    //         console.log(parentsInput);
-    //     }
-    let currentSpouseInput = parseInt(document.forms['all-criteria']['currentSpouse'].value);
+function searchByMultiple() {
+	let idInput = parseInt(document.forms['all-criteria']['id'].value);
+	let firstNameInput = document.forms['all-criteria']['fname'].value;
+	let lastNameInput = document.forms['all-criteria']['lname'].value;
+	let genderInput = document.forms['all-criteria']['gender'].value;
+	let dobInput = document.forms['all-criteria']['dob'].value;
+	let heightInput = parseInt(document.forms['all-criteria']['height'].value);
+	let weightInput = parseInt(document.forms['all-criteria']['weight'].value);
+	let eyeColorInput = document.forms['all-criteria']['eyeColor'].value;
+	let occupationInput = document.forms['all-criteria']['occupation'].value;
+	// let parentsInput = document.forms['all-criteria']['parents'].value;
+	//     if (parentsInput !== ""){
+	//         parentsInput = parentsInput.split(',');
+	//         let parentsInputNumArray = [];
+	//         for (let i=0; i<parentsInput.length; i++){
+	//             parentsInputNumArray.push(parseInt(parentsInput[i]))
+	//         }
+	//         parentsInput = parentsInputNumArray;
+	//         console.log(parentsInput);
+	//     }
+	let currentSpouseInput = parseInt(document.forms['all-criteria']['currentSpouse'].value);
 
-    let filteredPeople = people.filter(function(person){
-        if ((person.id === idInput || isNaN(idInput))
-            && (person.firstName === firstNameInput || firstNameInput == "")
-            && (person.lastName === lastNameInput || lastNameInput == "")
-            && (person.gender === genderInput || genderInput == "")
-            && (person.dob === dobInput || dobInput == "")
-            && (person.height == heightInput || isNaN(heightInput))
-            && (person.weight == weightInput || isNaN(weightInput))
-            && (person.eyeColor === eyeColorInput || eyeColorInput == "")
-            && (person.occupation == occupationInput || occupationInput == "")
-            //&& (person.parents ==parentsInput || parentsInput == "") // problem with array equality?
-            && (person.currentSpouse == currentSpouseInput || isNaN(currentSpouseInput))) 
-            {
-                return true;
-            }
-            return false;
-    })
-    if (filteredPeople.length > 0) {
-		fillTable(filteredPeople, "multi-search-table");
+	let filteredPeople = people.filter(function(person) {
+		if (
+			(person.id === idInput || isNaN(idInput)) &&
+			(person.firstName === firstNameInput || firstNameInput == '') &&
+			(person.lastName === lastNameInput || lastNameInput == '') &&
+			(person.gender === genderInput || genderInput == '') &&
+			(person.dob === dobInput || dobInput == '') &&
+			(person.height == heightInput || isNaN(heightInput)) &&
+			(person.weight == weightInput || isNaN(weightInput)) &&
+			(person.eyeColor === eyeColorInput || eyeColorInput == '') &&
+			(person.occupation == occupationInput || occupationInput == '') &&
+			//&& (person.parents ==parentsInput || parentsInput == "") // problem with array equality?
+			(person.currentSpouse == currentSpouseInput || isNaN(currentSpouseInput))
+		) {
+			return true;
+		}
+		return false;
+	});
+	if (filteredPeople.length > 0) {
+		fillTable(filteredPeople, 'multi-search-table');
 	} else {
 		alert('Sorry, looks like there is no one who matches that search.');
 	}
 }
 
+function searchDescendants(idInput) {
+	let descendants = [];
+	let children = people.filter(function(person) {
+		if (person.parents.includes(Number(idInput))) {
+			descendants = descendants.concat(searchDescendants(person['id']));
+			return true;
+		}
+		return false;
+	});
 
-function searchDescendants(idInput){
-    let descendants = [];
-    let children = people.filter(function(person){
-        if (person.parents.includes(Number(idInput))){
-            descendants = descendants.concat(searchDescendants(person["id"]));
-            return true
-        }
-        return false
-    })
-
-    descendants = descendants.concat(children);
-    return descendants;
+	descendants = descendants.concat(children);
+	return descendants;
 }
 
-function fillDescendantSearchTable(){
-    let searchInput = document.forms['descendant-search']['id'].value;
-    let results = searchDescendants(searchInput);
-    fillTable(results, "descendant-table");
+function fillDescendantSearchTable() {
+	let searchInput = document.forms['descendant-search']['id'].value;
+	let results = searchDescendants(searchInput);
+	fillTable(results, 'descendant-table');
 }
 
-function familySearch(idNumber){
-    let familyIDs = [];
-    let personObject = people.filter(function(person){
-        if (person.id == idNumber){
-            return true
-        }
-        return false
-    })
-    personObject = personObject[0];
-    let parentsIDArray = personObject.parents;
-    let spouseID = personObject['currentSpouse'];
-    let siblingsIDArray = [];
-    for (let i=0; i<parentsIDArray.length; i++){
-        people.filter(function(person){
-            if (person.parents.includes(parentsIDArray[i]) && person.id !== Number(idNumber)){
-                siblingsIDArray.push(person.id);
-                return true;
-            }
-            return false;
-        })
-    }
-    
-    // family = siblingsObjectArray.concat(spouseAndParents);
-    // console.log(family);
-    // fillTable(family, 'parents-table');
+function familySearch(idNumber) {
+	let familyIDs = [];
+	let personObject = people.filter(function(person) {
+		if (person.id == idNumber) {
+			return true;
+		}
+		return false;
+	});
+	personObject = personObject[0];
+	let parentsIDArray = personObject.parents;
+	let spouseID = personObject['currentSpouse'];
+	let siblingsIDArray = [];
+	for (let i = 0; i < parentsIDArray.length; i++) {
+		people.filter(function(person) {
+			if (person.parents.includes(parentsIDArray[i]) && person.id !== Number(idNumber)) {
+				siblingsIDArray.push(person.id);
+				return true;
+			}
+			return false;
+		});
+	}
+
+	// family = siblingsObjectArray.concat(spouseAndParents);
+	// console.log(family);
+	// fillTable(family, 'parents-table');
 }
 
 function fillTable(array, tableID) {
-    let heading = 
-    `<thead><tr>
+	let heading = `<thead><tr>
         <th>ID</th>
         <th>First Name</th>
         <th>Last Name</th>
@@ -124,7 +123,7 @@ function fillTable(array, tableID) {
         <th>Occupation</th>
         <th>Parents</th>
         <th>Current Spouse</th>
-    </tr></thead>`
+    </tr></thead>`;
 	let body = '';
 	array.map(function(object) {
 		body += `<tbody><tr>`;
@@ -142,10 +141,8 @@ const firstDiv = document.querySelector('.hidden');
 
 btnTwo.addEventListener('click', function() {
 	firstDiv.classList.toggle('hidden');
-	fillTable(people, "all-people");
+	fillTable(people, 'all-people');
 });
-
-// const me = [ { 'First Name': 'Eric', 'Last Name': 'Dude' } ];
 
 // //===Create thead function===
 // function createThead(table, data) {
@@ -171,8 +168,11 @@ btnTwo.addEventListener('click', function() {
 // 	}
 // }
 
+// const me = [ { 'First Name': 'Eric', 'Last Name': 'Dude' } ];
 // const table = document.querySelector('table');
 // let data = Object.keys(me[0]);
 
 // createTable(table, me);
 // createThead(table, data);
+
+// const table = document.querySelector('table').forEach(e => { e.remove });
